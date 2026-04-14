f5253f34338331e26cdd4b6f0c233ee884618b9a
Danya128 <oleynukd@gmail.com>
2026-04-15 00:29:23 +0100

    Update all buttons and creating random music

--- piano_ca.gd
+++ piano_ca.gd
@@ -3,6 +3,11 @@ extends Node2D
 var speed = 200
 var isOn_guitar = false
 var isOn_drums = false
+var isOn_random = false
+
+var random_delay = 0.4
+var random_timer = 0.0
+
 
 
 var all_keys = [
@@ -11,8 +16,13 @@ var all_keys = [
 	"BlackKey1", "BlackKey2", "BlackKey3", "BlackKey4"
 ]
 
+
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Guitar_part/guitarButton.modulate = Color(1.0, 0.098, 0.0, 1.0)
+	$Drums_part/drumsButton.modulate = Color(1.0, 0.098, 0.0, 1.0)
+	$Random/randomButton.modulate = Color(1.0, 0.098, 0.0, 1.0)
 	pass # Replace with function body.
 	
 
@@ -28,38 +38,25 @@ func spawn_note():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	var guitar_player = $Guitar_part/player_guitar
-	var drums_player = $Drums_part/player_drums
 	
 	#######################
-	# DRUMS 
+	# RANDOM FUNCTION
 	#######################
 	
-	if Input.is_action_just_pressed("Drums"):
-		if isOn_drums == true:
-			isOn_drums = false
-			drums_player.stop()
-			$Drums_part/drumsButton.modulate = Color(1.0, 1.0, 1.0, 1.0)
-		else:
-			isOn_drums = true
-			drums_player.play()
-			$Drums_part/drumsButton.modulate = Color(0.254, 0.591, 0.208, 1.0)
+	if isOn_random:
+		random_timer -= delta
+		if random_timer < 0:
+			random_timer = random_delay
+			var random_key = all_keys.pick_random()
 			
+			$Player.get_node(random_key + "_Player").play()
+			$Player.get_node(random_key + "_Player").volume_db = $SliderVolume.value
 			
-			
-	#######################
-	# GUITAR
-	#######################
+			$PianoKeys.get_node(random_key).modulate = Color(0.93, 0.0, 0.0, 1.0)
+			await get_tree().create_timer(0.2).timeout
+			$PianoKeys.get_node(random_key).modulate = Color(1.0, 1.0, 1.0, 1.0)
+			spawn_note()
 	
-	if Input.is_action_just_pressed("Guitar"):
-		if isOn_guitar == true:
-			isOn_guitar = false
-			guitar_player.stop()
-			$Guitar_part/guitarButton.modulate = Color(1.0, 1.0, 1.0, 1.0)
-		else:
-			isOn_guitar = true
-			guitar_player.play()
-			$Guitar_part/guitarButton.modulate = Color(0.254, 0.591, 0.208, 1.0)
 	
 	
 	#######################
@@ -145,3 +142,57 @@ func _process(delta: float) -> void:
 		spawn_note()
 		
 	pass
+	
+	#######################
+	# DRUMS 
+	#######################
+	
+func _on_button_pressed() -> void:
+	var drums_player = $Drums_part/player_drums
+	
+	if isOn_drums == true:
+		isOn_drums = false
+		drums_player.stop()
+		$Drums_part/drumsButton.modulate = Color(1.0, 0.098, 0.0, 1.0)
+	else:
+		isOn_drums = true
+		drums_player.play()
+		$Drums_part/drumsButton.modulate = Color(0.254, 0.591, 0.208, 1.0)
+			
+			
+	pass # Replace with function body.
+
+
+
+	#######################
+	# GUITAR
+	#######################
+
+func _on_button_2_pressed() -> void:
+	var guitar_player = $Guitar_part/player_guitar
+	
+	if isOn_guitar == true:
+		isOn_guitar = false
+		guitar_player.stop()
+		$Guitar_part/guitarButton.modulate = Color(1.0, 0.098, 0.0, 1.0)
+	else:
+		isOn_guitar = true
+		guitar_player.play()
+		$Guitar_part/guitarButton.modulate = Color(0.254, 0.591, 0.208, 1.0)
+	
+	pass # Replace with function body.
+	
+	
+	#######################
+	# Random
+	#######################
+	
+func _on_button_3_pressed() -> void:
+	if isOn_random == true:
+		isOn_random = false
+		$Random/randomButton.modulate = Color(1.0, 0.098, 0.0, 1.0)
+	else:
+		isOn_random = true
+		$Random/randomButton.modulate = Color(0.254, 0.591, 0.208, 1.0)
+	
+	pass # Replace with function body.
